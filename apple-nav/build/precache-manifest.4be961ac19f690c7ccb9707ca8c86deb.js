eee924e87a00f374519c016abb5786d1e3ae4c66
dmvazli <davidmvazquez1@gmail.com>
2019-06-09 23:20:24 -0400

    build

---
+++ apple-nav/build/precache-manifest.4be961ac19f690c7ccb9707ca8c86deb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc68d0d4757863f37b1db4af8d6e124",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5188cfefe59ca3ebdcb7",
+    "url": "/static/css/main.8dda2e25.chunk.css"
+  },
+  {
+    "revision": "e6c555c2fc000f5cabb9",
+    "url": "/static/js/2.62b10cd2.chunk.js"
+  },
+  {
+    "revision": "5188cfefe59ca3ebdcb7",
+    "url": "/static/js/main.54a7d7ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e8042105356dde9ed2c73226ed5c6a99",
+    "url": "/static/media/SF-Pro-Text-Light.e8042105.otf"
+  },
+  {
+    "revision": "c0443040caff9cab04670576397562bf",
+    "url": "/static/media/SF-Pro-Text-Medium.c0443040.otf"
+  }
+]);
